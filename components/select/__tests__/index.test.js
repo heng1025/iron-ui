f4d7bf9dabc399d2f41122f2810eff28930a3975
iron <472263356@qq.com>
2020-09-15 01:14:25 +0800

    :white_check_mark: update select unit test

--- components/select/__tests__/index.test.js
+++ components/select/__tests__/index.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { VirtualSelect } from '../index';
 
-const options = [];
+const options = ['abc', 'def', 'ccc', 'bsd'];
 
-for (let i = 0; i < 10000; i++) {
-  const value = `${i.toString(36)}${i}`;
-  options.push(value);
-}
+// for (let i = 0; i < 10000; i++) {
+//   const value = `${i.toString(36)}${i}`;
+//   options.push(value);
+// }
 
 describe('VirtualSelect', () => {
   function genSelect(args) {
@@ -27,5 +27,16 @@ describe('VirtualSelect', () => {
   it('renders disabled select correctly', () => {
     const wrapper = render(genSelect({ disabled: true }));
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should hide the dropdown when click outside', () => {
+    const wrapper = render(genSelect());
+    const inputEls = screen.getAllByPlaceholderText('please select');
+    // second input box
+    fireEvent(inputEls[1], new FocusEvent('focus'));
+    // screen.debug()
+    expect(wrapper.queryByText('abc')).toBeInTheDocument();
+    fireEvent.click(document);
+    expect(wrapper.queryByText('abc')).not.toBeInTheDocument();
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   verbose: true,
+  setupFilesAfterEnv: ['./jest.setup.js'],
   // includes deps
-  transformIgnorePatterns: ['node_modules/(?!(react-virtualized|antd|rc-*|css-animation))'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-virtualized|antd|rc-*|css-animation))',
+  ],
   transform: {
     '\\.jsx?$': './script/codePreprocessor',
   },
