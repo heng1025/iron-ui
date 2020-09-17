45f9a582c09aac1a371fe4c4925cf1ed05392d25
iron <472263356@qq.com>
2020-09-17 19:38:34 +0800

    :children_crossing: update icon position

--- components/input/index.jsx
+++ components/input/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import classNames from 'classnames';
 
-const Input = ({ suffix, ...rest }) => {
+const Input = forwardRef(({ suffix, ...rest }, ref) => {
   return (
     <div className="iron-input-wrapper">
-      <input {...rest} className="iron-input" />
+      <input
+        {...rest}
+        ref={ref}
+        className={classNames('iron-input', suffix ? 'iron-input-icon' : '')}
+      />
       {suffix && <span className="iron-input-suffix">{suffix}</span>}
     </div>
   );
-};
+});
 
 export default Input;
