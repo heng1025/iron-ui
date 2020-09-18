2939d757ccc2789db3afc0914723178bd35fdbab
iron <472263356@qq.com>
2020-09-19 01:00:15 +0800

    :alien: add comp to entry

--- components/index.jsx
+++ components/index.jsx
@@ -2,4 +2,8 @@
 export { default as VirtualSelect } from './select';
 export { default as VirtualAutoComplete } from './auto-complete';
 export { default as VirtualTree } from './tree';
+export { default as Icon } from './icon';
 export { default as Input } from './input';
+export { default as Checkbox } from './checkbox';
+export { default as Empty } from './empty';
+export { default as Spin } from './spin';
